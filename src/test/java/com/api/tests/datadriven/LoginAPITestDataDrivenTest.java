bd2bc5dee9907d07d9ccf44ecb0e629a1dd9e03b
Tushar A <tushararbansdet@gmail.com>
2026-09-02 00:11:47 +0530

    Introduced LoginAPI Data driven test for POC of open CSV Library!
    Introduced the dataprovider util to pass the test data to loginapitest
    and the tests will be executed parallel!

---
+++ src/test/java/com/api/tests/datadriven/LoginAPITestDataDrivenTest.java
@@ -0,0 +1,37 @@
+package com.api.tests.datadriven;
+
+import static com.api.utils.SpecUtil.requestSpec;
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.api.request.model.UserCredentials;
+import com.dataproviders.api.bean.UserBean;
+
+public class LoginAPITestDataDrivenTest {
+
+
+	@Test(description = "Verifying if login api is working for FD user",
+			groups = { "api", "regression", "datadriven" },
+			dataProviderClass = com.dataproviders.DataProviderUtils.class,
+			dataProvider = "LoginAPIDataProvider")
+	public void loginAPITest(UserBean userBean) {
+
+		given()
+		.spec(requestSpec(userBean))
+		.when()
+		.post("login")
+		.then()
+		.spec(responseSpec_OK())
+		.body("message", equalTo("Success"))
+		.and()
+		.body(matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+	}
+
+}
